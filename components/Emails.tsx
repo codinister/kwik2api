c7c1ba6a1d074a071dc36239d7efa6b227320573
unknown <codinister@gmail.com>
2025-01-17 13:22:52 +0100

    Commit 7

--- components/Emails.tsx
+++ components/Emails.tsx
@@ -117,7 +117,7 @@ const code = {
   fontFamily: 'HelveticaNeue-Bold',
   fontSize: '16px',
   fontWeight: 700,
-  lineHeight: '24px',
+  lineHeight: '20px',
   paddingBottom: '8px',
   paddingTop: '8px',
   margin: '0 auto',
